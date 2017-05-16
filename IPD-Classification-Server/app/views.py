dd3a82d69ef7ddfe18a0c0cc23e64d3b27ae8bbb
Shouvik Mani <shouvikm@andrew.cmu.edu>
2017-05-16 14:34:01 -0700

    Update classification server with new classifiers

--- IPD-Classification-Server/app/views.py
+++ IPD-Classification-Server/app/views.py
@@ -14,7 +14,7 @@ def classifyPotholes(request):
     rawSensorData = request.POST['sensorData']
     rawSensorData = rawSensorData.replace("'", '"')   # Standardize request data
     rawSensorDF = pd.read_json(rawSensorData, orient='records')
-    sensorAggregatesDF = getSensorIntervalAggregates(rawSensorDF)
+    sensorAggregatesDF = getSensorIntervalAggregates2(rawSensorDF)
     classifierFilePath = os.path.join(settings.STATIC_ROOT, 'potholeClassifier.p')
     classifier = pickle.load(open(classifierFilePath, 'rb'))
     clf = classifier['classifier']
@@ -28,14 +28,14 @@ def classifyRoadConditions(request):
     rawSensorData = request.POST['sensorData']
     rawSensorData = rawSensorData.replace("'", '"')   # Standardize request data
     rawSensorDF = pd.read_json(rawSensorData, orient='records')
-    sensorAggregatesDF = getSensorIntervalAggregates(rawSensorDF)
+    sensorAggregatesDF = getSensorIntervalAggregates1(rawSensorDF)
     classifierFilePath = os.path.join(settings.STATIC_ROOT, 'roadConditionsClassifier.p')
     classifier = pickle.load(open(classifierFilePath, 'rb'))
     clf = classifier['classifier']
     y_pred = clf.predict(sensorAggregatesDF)
     return HttpResponse(y_pred)
 
-def getSensorIntervalAggregates(rawSensorDF):
+def getSensorIntervalAggregates1(rawSensorDF):
     meanSpeed, sdSpeed = rawSensorDF['speed'].mean(), rawSensorDF['speed'].std()
     maxAccelX, minAccelX = rawSensorDF['accelerometerX'].max(), rawSensorDF['accelerometerX'].min()
     maxAccelY, minAccelY = rawSensorDF['accelerometerY'].max(), rawSensorDF['accelerometerY'].min()
@@ -50,5 +50,22 @@ def getSensorIntervalAggregates(rawSensorDF):
     meanGyroY, sdGyroY = rawSensorDF['gyroY'].mean(), rawSensorDF['gyroY'].std()
     meanGyroZ, sdGyroZ = rawSensorDF['gyroZ'].mean(), rawSensorDF['gyroZ'].std()
     intervalAggregates = [[meanSpeed, sdSpeed, maxAccelX, maxAccelY, maxAccelZ,
+                            maxGyroX, maxGyroY, maxGyroZ, minAccelX, minAccelY, minAccelZ, minGyroX, minGyroY,minGyroZ, meanAccelX, meanAccelY, meanAccelZ, meanGyroX, meanGyroY, meanGyroZ,sdAccelX, sdAccelY, sdAccelZ, sdGyroX, sdGyroY, sdGyroZ]]
+    return intervalAggregates
+
+def getSensorIntervalAggregates2(rawSensorDF):
+    maxAccelX, minAccelX = rawSensorDF['accelerometerX'].max(), rawSensorDF['accelerometerX'].min()
+    maxAccelY, minAccelY = rawSensorDF['accelerometerY'].max(), rawSensorDF['accelerometerY'].min()
+    maxAccelZ, minAccelZ = rawSensorDF['accelerometerZ'].max(), rawSensorDF['accelerometerZ'].min()
+    maxGyroX, minGyroX = rawSensorDF['gyroX'].max(), rawSensorDF['gyroX'].min()
+    maxGyroY, minGyroY = rawSensorDF['gyroY'].max(), rawSensorDF['gyroY'].min()
+    maxGyroZ, minGyroZ = rawSensorDF['gyroZ'].max(), rawSensorDF['gyroZ'].min()
+    meanAccelX, sdAccelX = rawSensorDF['accelerometerX'].mean(), rawSensorDF['accelerometerX'].std()
+    meanAccelY, sdAccelY = rawSensorDF['accelerometerY'].mean(), rawSensorDF['accelerometerY'].std()
+    meanAccelZ, sdAccelZ = rawSensorDF['accelerometerZ'].mean(), rawSensorDF['accelerometerZ'].std()
+    meanGyroX, sdGyroX = rawSensorDF['gyroX'].mean(), rawSensorDF['gyroX'].std()
+    meanGyroY, sdGyroY = rawSensorDF['gyroY'].mean(), rawSensorDF['gyroY'].std()
+    meanGyroZ, sdGyroZ = rawSensorDF['gyroZ'].mean(), rawSensorDF['gyroZ'].std()
+    intervalAggregates = [[maxAccelX, maxAccelY, maxAccelZ,
                             maxGyroX, maxGyroY, maxGyroZ, minAccelX, minAccelY, minAccelZ, minGyroX, minGyroY,minGyroZ, meanAccelX, meanAccelY, meanAccelZ, meanGyroX, meanGyroY, meanGyroZ,sdAccelX, sdAccelY, sdAccelZ, sdGyroX, sdGyroY, sdGyroZ]]
     return intervalAggregates
